726826008f03a7cac5d3382b8723c8c8ca8222f4
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-17 13:24:23 -0300

    implemented contact controller

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,10 +1,30 @@
 const {Contact} = require('../models')
 const createContact = async (req, res, _next) => {
-    const contacts = await Contact.findOne({})
-    console.log(contacts)
-    res.send('contact')
+  const {name,phone, email, message } = req.body
+  
+  try{
+    const newContact = await Contact.create({
+        name, 
+        email,
+        phone: phone || null,
+        message: message || null
+      })
+    res.status(200).send(newContact)
+  }catch (err){
+    res.status(400).send(err)
+  }
+    
+}
+const getContacts = async( req, res, _next) => {
+    try{
+        const contacts = await Contact.findAll()
+        res.status(200).send(contacts)
+    }catch (err){
+        res.status(400).send(err)
+    }
 }
 
 module.exports = {
-    createContact
+    createContact,
+    getContacts
 }
