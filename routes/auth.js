c81e0409a90e845a54b4809213c567474acfd6eb
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-17 10:55:14 -0300

    implemented validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const { registerUser, loginUser } = require("../controllers/users");
-
+const {registerValidator, loginValidator} = require('../validators/users')
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", registerValidator, registerUser);
 
-router.post("/login", loginUser);
+router.post("/login", loginValidator, loginUser);
 
 module.exports = router;
