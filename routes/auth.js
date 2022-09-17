c47e259d137d96c1d4073c0438a14d0a52010c7b
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-17 08:34:35 -0300

    Implemented auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,11 @@
-const express = require('express');
-const sequelize = require('sequelize');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { registerUser, loginUser } = require("../controllers/userController");
 
 const router = express.Router();
-const { User, Role } = require('../models');
-router.post('/register', (req, res, next) => {
 
-    
-    
-    const {firstName, lastName, email, password, roleId, image} = req.body;
-    const userExist = User.findOne({where: {email: email}});
-    if(userExist){
-        res.send('User already exist');
-    }
-    else{
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            password,
-            image,
-            roleId: roleId || 2
-        });
-        
-        newUser.save()
-        res.send(newUser)
-    }
-    
-}) 
+router.post("/register", registerUser);
 
-router.post('/login', (req, res, _next) => {
-    res.send('Login');
-})
-
-
-
+router.post("/login", loginUser);
 
 module.exports = router;
