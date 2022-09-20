e3f8a6ba4dc1cbff5cecacc879d19cfb88dedad0
Marco Romero <marcod.romero@gmail.com>
2022-09-20 05:30:05 -0300

    Create the category creation endpoint

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {getAllCategories} = require('../controllers/categories');
+const {getAllCategories, addCategory} = require('../controllers/categories');
 
 router.get('/', function(req, res, next){
     getAllCategories(res);
 });
+
+router.post('/', addCategory);
 
 module.exports = router;
