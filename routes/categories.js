979c4d8351549acf1d36c2c05e11f9dd450f6665
Marco Romero <marcod.romero@gmail.com>
2022-09-20 15:11:40 -0300

    Create endpoint delete categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {getAllCategories} = require('../controllers/categories');
+const {getAllCategories, deleteCategory} = require('../controllers/categories');
 
 router.get('/', function(req, res, next){
     getAllCategories(res);
 });
+
+router.delete('/:id', deleteCategory);
 
 module.exports = router;
