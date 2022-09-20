36e4c12ec0c0bcf9a568f6a0ffdf0f6c27843a5b
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-20 20:55:10 -0300

    Fix bad require

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -1,9 +1,9 @@
-const Testimonial = require('../models');
+const {Testimonial} = require('../models');
 const createTestimonial = async (req, res, _next) => {
     const {name, image, content } = req.body;
     const testimonial = await Testimonial.create({name, image, content});
     res.send(testimonial);
 }
 
-module.exports = createTestimonial
+module.exports = {createTestimonial}
     
