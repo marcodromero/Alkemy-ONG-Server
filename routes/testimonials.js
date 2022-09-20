9de8d53570f73e28e126f36aba1d6de1e52e4143
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-20 20:29:14 -0300

    create testimonials route

---
+++ routes/testimonials.js
@@ -0,0 +1,8 @@
+var express = require('express');
+const verifyRole = require('../middlewares/verifyRole')
+const createTestimonial = require('../controllers/testimonials')
+const { testimonialValidator } = require('../validators/testimonials')
+var router = express.Router();
+router.post('/', verifyRole , testimonialValidator,  createTestimonial);
+
+module.exports = router;
