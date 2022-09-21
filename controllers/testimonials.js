1b05e3d8965cba6787526f913c572baa74a37aa4
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-20 22:30:58 -0300

    implemented delete testimonial endpoint

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -4,6 +4,19 @@ const createTestimonial = async (req, res, _next) => {
     const testimonial = await Testimonial.create({name, image, content});
     res.send(testimonial);
 }
-
-module.exports = {createTestimonial}
+const deleteTestimonial = async (req, res, _next) => {
+    const {id} = req.params;
+    const testimonial = await Testimonial.findOne({where: {id}});
+    if(!testimonial) {
+        res.status(404).send('Testimonial not found');
+    }else{
+        try{
+            await testimonial.destroy();
+            res.status(200).send('Testimonial deleted');
+        }catch(err){
+            res.status(500).send('Error deleting testimonial');
+        }
+    }
+}
+module.exports = {createTestimonial, deleteTestimonial}
     
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,8 +1,9 @@
 var express = require('express');
 const verifyRole = require('../middlewares/verifyRole')
-const {createTestimonial} = require('../controllers/testimonials')
+const {createTestimonial, deleteTestimonial} = require('../controllers/testimonials')
 const { testimonialValidator } = require('../validators/testimonials')
 var router = express.Router();
 router.post('/', verifyRole , testimonialValidator,  createTestimonial);
+router.delete('/:id', verifyRole , deleteTestimonial);
 
 module.exports = router;
