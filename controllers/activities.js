0b207cf26657f1a6ae25c93d98d7c4c3a2498f0b
FelipeLL <f3lip3.3z@hotmail.com>
2022-09-20 20:13:23 -0500

    created endpoint for update activities

--- controllers/activities.js
+++ controllers/activities.js
@@ -14,7 +14,30 @@ const createActivity = async (req, res, _next) => {
     }
 }
 
+const updateActivity = async (req, res, _next) => {
+    try {
+        const { id } = req.params
+        const { name, image, content } = req.body
+        const activityExist = await Activity.findOne({ where: { id } })
+        if (!activityExist) {
+            throw "Activity doesn't exist"
+        } else {
+            await Activity.update({
+                name,
+                image,
+                content
+            }, {
+                where: { id }
+            })
+            res.send("Activity has been updated")
+        }
+
+    } catch (err) {
+        res.status(400).send(err)
+    }
+}
 
 module.exports = {
-    createActivity
+    createActivity, updateActivity
 };
+
