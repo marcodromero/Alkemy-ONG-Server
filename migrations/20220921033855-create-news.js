8a6cd43e52b850c17d1daeeade51966a547182bb
Robert Santos <rssp_96@hotmail.com>
2022-09-21 12:17:26 -0500

    OT288-41

---
+++ migrations/20220921033855-create-news.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('News', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },      
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('News');
+  }
+};
