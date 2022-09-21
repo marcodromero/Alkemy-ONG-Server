833a65ce0523d3c4ecc21dd2bd3ea6578be097aa
Robert Santos <rssp_96@hotmail.com>
2022-09-21 14:17:45 -0500

    enpoint

--- controllers/news.js
+++ controllers/news.js
@@ -2,7 +2,7 @@ const { News } = require("../models");
 
 const findAllNews = async (req, res) => {
   try {
-    const news = await News.findAll({});
+    const news = await News.findAll({ where: { type: "news" } });
 
     return res.status(200).json(news);
   } catch (error) {
