48968a95a9bc465c6fbd608e727892ace831e347
FelipeLL <f3lip3.3z@hotmail.com>
2022-09-26 22:19:25 -0500

    Fix member model

--- models/member.js
+++ models/member.js
@@ -1,26 +1,26 @@
-const {Model} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Member extends Model {
-      /**
-       * Helper method for defining associations.
-       * This method is not a part of Sequelize lifecycle.
-       * The `models/index` file will call this method automatically.
-       */
-      static associate(models) {
-      }
+  class Member extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+    }
 
-    };
-    Member.init({
-        id:{
-          type:DataTypes.INTEGER,
-          autoIncrement:true,
-          primaryKey:true
-        },
-        name: DataTypes.STRING,
-        image: DataTypes.STRING
-    }, {
-      sequelize,
-      modelName: 'Members',
-    });
-    return Member;
+  };
+  Member.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Member',
+  });
+  return Member;
 };
