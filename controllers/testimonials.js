24591ad0eaab6559b5034094fab7c83fa55bad57
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-28 02:47:17 -0300

    implemented update testimonial

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -1,9 +1,31 @@
-const {Testimonial} = require('../models');
+const { Testimonial } = require("../models");
 const createTestimonial = async (req, res, _next) => {
-    const {name, image, content } = req.body;
-    const testimonial = await Testimonial.create({name, image, content});
-    res.send(testimonial);
-}
-
-module.exports = {createTestimonial}
-    
+  const { name, image, content } = req.body;
+  const testimonial = await Testimonial.create({ name, image, content });
+  res.send(testimonial);
+};
+const updateTestimonial = async (req, res, _next) => {
+  const { id } = req.params;
+  const { name, image, content } = req.body;
+  try {
+    const update = await Testimonial.update(
+      {
+        name,
+        image,
+        content,
+      },
+      {
+        where: { id: id },
+      }
+      );
+    res.status(200).send({
+      data: { name, image, content },
+    });
+  } catch (error) {
+    res.status(400).send({ error:error });
+  }
+};
+module.exports = {
+  createTestimonial,
+  updateTestimonial,
+};
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,8 +1,9 @@
 var express = require('express');
 const verifyRole = require('../middlewares/verifyRole')
-const {createTestimonial} = require('../controllers/testimonials')
+const {createTestimonial, updateTestimonial} = require('../controllers/testimonials')
 const { testimonialValidator } = require('../validators/testimonials')
 var router = express.Router();
 router.post('/', verifyRole , testimonialValidator,  createTestimonial);
+router.put('/:id' , verifyRole, updateTestimonial )
 
 module.exports = router;
