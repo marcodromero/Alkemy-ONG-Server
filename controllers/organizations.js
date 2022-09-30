b11d45cee3e6d8bd8528b37570f988b7830ed574
FelipeLL <f3lip3.3z@hotmail.com>
2022-09-30 13:13:19 -0500

    Created endpoint for update organization

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -11,6 +11,34 @@ const getOrganization = async (req, res, _next) => {
 
 };
 
+const updateOrganization = async (req, res, _next) => {
+    try {
+        const { title, description, image } = req.body
+        const existOrganization = await Organization.findOne()
+        if (!existOrganization) {
+            throw "Organization doesn't exist"
+        } else {
+            const organization = {
+                id: existOrganization.id,
+                title,
+                description,
+                image,
+                facebook: existOrganization.facebook,
+                linkedin: existOrganization.linkedin,
+                instagram: existOrganization.instagram
+            }
+            await Organization.update(organization, {
+                where: { id: organization.id }
+            })
+            res.json(organization)
+        }
+    } catch (error) {
+        res.status(400).send(err)
+    }
+
+}
+
 module.exports = {
     getOrganization,
+    updateOrganization
 }
--- routes/organizations.js
+++ routes/organizations.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getOrganization } = require("../controllers/organizations");
+const { getOrganization, updateOrganization } = require("../controllers/organizations");
 const router = express.Router();
 
 router.get("/", getOrganization);
+router.put("/", updateOrganization)
 
 module.exports = router;
