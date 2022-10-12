7a22a371d3217c32dba012d4cf1a2eff57f8e631
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-11 23:56:19 -0300

    added GET activities/id endpoint

--- routes/activities.js
+++ routes/activities.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createActivity, updateActivity } = require("../controllers/activities");
+const { createActivity, updateActivity, getActivity } = require("../controllers/activities");
 const { createValidator } = require("../validators/activities")
 const router = express.Router();
 
+router.get('/:id', getActivity)
 router.post("/", createValidator, createActivity);
 router.put("/:id", updateActivity);
 
