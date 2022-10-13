746a37cdc61568ae91a9e36acc54ba63855bd5dd
FelipeLL <f3lip3.3z@hotmail.com>
2022-10-13 07:55:40 -0500

    Created testimonial endpoint

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,10 +1,10 @@
 var express = require('express');
 const verifyRole = require('../middlewares/verifyRole')
-const {createTestimonial, deleteTestimonial, updateTestimonial} = require('../controllers/testimonials')
+const { createTestimonial, deleteTestimonial, updateTestimonial, getAllTestimonials } = require('../controllers/testimonials')
 const { testimonialValidator } = require('../validators/testimonials')
 var router = express.Router();
-router.post('/', verifyRole , testimonialValidator,  createTestimonial);
-router.put('/:id', verifyRole , updateTestimonial);
-router.delete('/:id', verifyRole , deleteTestimonial);
-
+router.post('/', verifyRole, testimonialValidator, createTestimonial);
+router.put('/:id', verifyRole, updateTestimonial);
+router.delete('/:id', verifyRole, deleteTestimonial);
+router.get("/", getAllTestimonials)
 module.exports = router;
