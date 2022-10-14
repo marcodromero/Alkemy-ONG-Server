2076b176fc965cdaa7dedfaf76e5070f05af47bc
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-14 18:12:07 -0300

    Implemented GET user by id endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
-const { getUsers } = require('../controllers/users');
+const { getUsers, getUser } = require('../controllers/users');
 const verifyRole = require('../middlewares/verifyRole');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/',verifyRole ,getUsers);
+router.get('/',verifyRole ,getUsers)
+router.get('/:id',verifyRole ,getUser)
 
 module.exports = router;
