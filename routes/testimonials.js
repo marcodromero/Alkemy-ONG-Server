bc212c8117ecd5391a79d8533119596557a15211
FelipeLL <f3lip3.3z@hotmail.com>
2022-10-18 23:00:58 -0500

    Fix testimonial routes

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -3,8 +3,8 @@ const verifyRole = require('../middlewares/verifyRole')
 const { createTestimonial, deleteTestimonial, updateTestimonial, getAllTestimonials } = require('../controllers/testimonials')
 const { testimonialValidator } = require('../validators/testimonials')
 var router = express.Router();
-router.post('/', verifyRole, testimonialValidator, createTestimonial);
-router.put('/:id', verifyRole, updateTestimonial);
-router.delete('/:id', verifyRole, deleteTestimonial);
+router.post('/', testimonialValidator, createTestimonial);
+router.put('/:id', updateTestimonial);
+router.delete('/:id', deleteTestimonial);
 router.get("/", getAllTestimonials)
 module.exports = router;
