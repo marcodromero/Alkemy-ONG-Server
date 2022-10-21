8adb2d0d7b45c60fedb67979a15855071ee1827f
FelipeLL <f3lip3.3z@hotmail.com>
2022-10-21 12:38:56 -0500

    Created delete contact endpoint

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {createContact, getContacts} = require('../controllers/contacts')
-const {contactValidator} = require('../validators/contacts')
+const { createContact, getContacts, deleteContact } = require('../controllers/contacts')
+const { contactValidator } = require('../validators/contacts')
 const verifyRole = require('../middlewares/verifyRole')
 /* GET home page. */
-router.post('/', contactValidator ,createContact);
-router.get('/' , verifyRole, getContacts)
+router.post('/', contactValidator, createContact);
+router.get('/', getContacts)
+router.delete('/:id', deleteContact)
 
 module.exports = router;
