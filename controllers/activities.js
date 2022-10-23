2923371ef7aa169929ce16f0d367d4add220dd99
FelipeLL <f3lip3.3z@hotmail.com>
2022-10-22 20:35:10 -0500

    Created delete activities endpoint

--- controllers/activities.js
+++ controllers/activities.js
@@ -2,28 +2,28 @@ const { Activity } = require("../models")
 
 
 const getActivity = async (req, res, _next) => {
-    try{
-        const {id } = req.params
-        const activity = await Activity.findOne({where: {id: id}})
-        if(activity){
+    try {
+        const { id } = req.params
+        const activity = await Activity.findOne({ where: { id: id } })
+        if (activity) {
             res.status(200).send(activity)
-        }else{
-            res.status(404).send({'error': 'activity not found'})
+        } else {
+            res.status(404).send({ 'error': 'activity not found' })
         }
-    }catch(e){
-        res.status(400).send({'error': e})
+    } catch (e) {
+        res.status(400).send({ 'error': e })
     }
 }
 const getActivities = async (_req, res, _next) => {
-    try{
+    try {
         const activities = await Activity.findAll()
-        if(activities){
+        if (activities) {
             res.status(200).send(activities)
-        }else{
-            res.status(404).send({'error': 'no activities where found'})
+        } else {
+            res.status(404).send({ 'error': 'no activities where found' })
         }
-    }catch(e){
-        res.status(400).send({'error': e})
+    } catch (e) {
+        res.status(400).send({ 'error': e })
     }
 }
 const createActivity = async (req, res, _next) => {
@@ -65,7 +65,26 @@ const updateActivity = async (req, res, _next) => {
     }
 }
 
+const deleteActivity = async (req, res, _next) => {
+    try {
+        const { id } = req.params
+        const activityExist = await Activity.findOne({ where: { id } })
+        if (!activityExist) {
+            throw "Activity doesn't exist"
+        } else {
+
+            await Activity.destroy({
+                where: { id }
+            })
+            res.send(activityExist)
+        }
+
+    } catch (err) {
+        res.status(400).send(err)
+    }
+};
+
 module.exports = {
-    createActivity, updateActivity, getActivity, getActivities
+    createActivity, updateActivity, getActivity, getActivities, deleteActivity
 };
 
