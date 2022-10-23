1fe09a51f4726dc8637ce02049aca669d854dd5b
FelipeLL <f3lip3.3z@hotmail.com>
2022-10-23 15:40:33 -0500

    Changed model and seeder Member

--- migrations/202216091436-create-member.js
+++ migrations/202216091436-create-member.js
@@ -1,32 +1,31 @@
 module.exports = {
-    up: async (queryInterface,Sequelize) => {
+    up: async (queryInterface, Sequelize) => {
         await queryInterface.createTable("Members",
-        {
-            id:{
-                type:Sequelize.INTEGER,
-                primaryKey:true,
-                autoIncrement:true
-            },
-            name:{
-                type:Sequelize.STRING,
-                allowNull:false 
-            },
-            image:{
-                type: Sequelize.STRING,
-                allowNull:true //Discutir en Daily
-            },
-            createdAt: {
-              allowNull: false,
-              type: Sequelize.DATE
-            },
-            updatedAt: {
-              allowNull: false,
-              type: Sequelize.DATE
+            {
+                id: {
+                    type: Sequelize.INTEGER,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                name: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                image: {
+                    type: Sequelize.BLOB("long"),
+                },
+                createdAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                },
+                updatedAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                }
             }
-        }
         )
     },
-    down: async (queryInterface) =>{
+    down: async (queryInterface) => {
         await queryInterface.dropTable('Members');
     }
 }
